8c5f9887328db8823c281a2e8e20251e35cc385e
yuriwillian <yuriwilian@gmail.com>
2022-06-19 17:27:46 -0300

    calculadora-de-receita

--- main.js
+++ main.js
@@ -9,23 +9,39 @@ function adicionarIngrediente() {
     let quantidadeEmbalagem = document.getElementById("quantidadeEmbalagem")
     let tipoQuantidade = document.getElementById("tipoQuantidade")
     let quantidadeUsada = document.getElementById("quantidadeUsada")
+    
+    if(nomeIngrediente.value != "" && custoEmbalagem.value != "" && quantidadeEmbalagem.value != "" && tipoQuantidade.value != "" && quantidadeUsada != ""){
+    
+        if(idIngrediente < 1){
+            let btnReceita = document.getElementById("btnReceita")
+            btnReceita.innerHTML = `
+            <input type="button" value="Calcular receita" onclick="calcularReceita()" class="btnForm"> 
+            `
+        }
 
     let custoIngrediente = custoEmbalagem.value / quantidadeEmbalagem.value * quantidadeUsada.value
 
     receita.push(custoIngrediente)
     ingredientes.innerHTML += `
-    <br><br>
-    <div id="${idIngrediente}">
-    ${nomeIngrediente.value} 
-    <br>Custo embalagem: ${custoEmbalagem.value} 
+    <div id="${idIngrediente}" class="ingrediente">
+    <strong>${nomeIngrediente.value}</strong>
+    <br>Custo embalagem: R$${custoEmbalagem.value} 
     <br> Quantidade da embalagem: ${quantidadeEmbalagem.value} ${tipoQuantidade.value}
     <br> Quantidade utilizada: ${quantidadeUsada.value} ${tipoQuantidade.value}
-    <br> Custo da quantidade utilizada: ${custoIngrediente}
+    <br> Custo da quantidade utilizada: <strong>R$${custoIngrediente}</strong>
     <div>`
 
     console.log(`ID Ingrediente atual: ${idIngrediente}`)
-
+    
+    nomeIngrediente.value = ""
+    custoEmbalagem.value = ""
+    quantidadeEmbalagem.value = ""
+    tipoQuantidade.value = ""
+    quantidadeUsada.value = ""
     return idIngrediente += 1
+    } else{
+        window.alert("Todos os campos são obrigatórios.")
+    }
 
 }
 
@@ -44,5 +60,5 @@ function calcularReceita() {
         resultado += receita[i]
     }
     let custoReceita = document.getElementById("custoReceita")
-    custoReceita.innerHTML = `<p> O custo total da receita é: <strong>R$ ${resultado}</strong> <p>`
+    custoReceita.innerHTML = `<p class="resultado"> <strong>O custo total da receita é: R$ ${resultado}</strong> <p>`
 }
